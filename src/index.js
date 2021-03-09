8d9ce8adee228e0fbaba5d403bf4fd9304952265
Jahaira-Marrero <jahaira.marrero@gmail.com>
2021-03-09 13:56:51 -0500

    UserProfile

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const header = document.querySelector('header')
+
+function renderProfile() {
+    fetch('http://localhost:3000/users/1')
+    .then(response => response.json())
+    .then(user => {
+        header.innerHTML = `<h1> ${user.username} </h1>
+        <h3> ${user.movies.forEach(movie => {
+            renderMovie(movie)
+        })
+        })} </h3>`
+
+        })
+    }
 
 function renderMovies() {
     fetch('http://localhost:3000/movies')
@@ -20,4 +34,5 @@ function renderMovie(movie) {
     moviediv.append(div)
 }
 
+renderProfile()
 renderMovies()
