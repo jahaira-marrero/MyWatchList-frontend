d176a3fe25a17f5d9bccecbdb989f493fb99d959
Dayne Dewar <daynedewar@yahoo.com>
2021-03-11 13:25:49 -0500

    cleaned up for Jahaira

--- src/index.js
+++ src/index.js
@@ -1,194 +1,17 @@
+/********** Global Variables **********/
+
 const header = document.querySelector('header')
 const headerName = header.querySelector('h1')
-const profileId = 1
 const myMovieDiv = document.querySelector('div#movie-card-collection')
+const searchForm = document.querySelector('form#movie-search-form')
+const loginForm = document.querySelector('form#login')
+let profileId = 2
 let movies = []
 let myMovies = []
-const searchForm = document.querySelector('#movie-search-form')
 
-// function renderProfile(profileId) {
-    // fetch(`http://localhost:3000/users/${profileId}`)
-    // .then(response => response.json())
-    // .then(userHash => {
-    //     headerName.innerText = `Profile: ${userHash.username}`
+/********** Functions **********/
 
-    //     if (userHash.netflix === true) {
-    //         const netflixLogo = document.createElement('img')
-    //         netflixLogo.dataset.id = 1
-    //         netflixLogo.src = "https://cdn.iconscout.com/icon/free/png-256/netflix-282224.png"
-    //         header.append(netflixLogo)
-    //         netflixLogo.addEventListener('click', event => {
-    //             myMovieDiv.innerHTML = ""
-    //             userHash.user_movies.forEach(userMovie => {
-    //                 if (userMovie.movie.netflix == true) {
-    //                     const netflixDiv = document.createElement('div')
-    //                     netflixDiv.classList.add('netflixcard')
-    //                     netflixDiv.dataset.id = userMovie.movie.id
-    //                     netflixDiv.innerHTML =`<h2> ${userMovie.movie.title} </h2>
-    //                     <h3> Year Released: ${userMovie.movie.year} </h3>
-    //                     <img src ="${userMovie.movie.image}">
-    //                     <p>${userMovie.movie.likes}</p> 
-    //                     <button class="like-btn">❤️</button> 
-    //                     <span> Dislikes: ${userMovie.movie.dislikes}</span>
-    //                     <button class="dislike-btn">👎🏾</button>
-    //                     <button class="delete-btn">✖️</button>
-    //                     `
-    //                     myMovieDiv.append(netflixDiv)
-
-    //                     const likeBtn = netflixDiv.querySelector('.like-btn')
-    //                     const dislikeBtn = netflixDiv.querySelector('.dislike-btn')
-    //                     const deleteBtn = netflixDiv.querySelector('.delete-btn')
-
-    //                     document.addEventListener('click', event => {
-    //                         if (event.target.className === 'like-btn') {
-    //                             console.log(event.target.parentNode)
-    //                         const likes = parseInt(userMovie.movie.likes) + 1
-    //                         fetch(`http://localhost:3000/movies/${event.target.parentNode.dataset.id}`, {
-    //                             method: "PATCH",
-    //                             headers: {
-    //                                 "Content-Type": "application/json"
-    //                             },
-    //                             body: JSON.stringify({likes})
-    //                         })
-    //                         .then(response => response.json())
-    //                         .then(movie => {
-    //                             const likeTag = event.target.parentNode.querySelector('p')
-    //                             likeTag.textContent = movie.likes
-    //                         })}
-    //                 })
-
-    //                     document.addEventListener('click', event => {
-    //                         if (event.target.className === 'dislike-btn') {
-    //                         const dislikes = parseInt(userMovie.movie.dislikes) + 1
-    //                         fetch(`http://localhost:3000/movies/${event.target.parentNode.dataset.id}`, {
-    //                             method: "PATCH",
-    //                             headers: {
-    //                                 "Content-Type": "application/json"
-    //                             },
-    //                             body: JSON.stringify({dislikes})
-    //                         })
-    //                         .then(response => response.json())
-    //                         .then(movie => {
-    //                             const dislikeTag = document.querySelector('span')
-    //                             dislikeTag.textContent = `Dislikes: ${movie.dislikes}`
-    //                         })}
-    //                     })
-
-    //                     document.addEventListener('click', event => {
-    //                         if (event.target.className === 'delete-btn') {
-    //                         const cardDiv = event.target.parentNode
-    //                         fetch(`http://localhost:3000/user_movies/${userMovie.id}`, {
-    //                             method: "DELETE",
-    //                             headers: {
-    //                                 "Content-Type": "application/json"
-    //                             }})
-    //                             .then(response => response.json())
-    //                             .then(data => cardDiv.remove())
-    //                     }})
-    //                 }
-    //             })
-
-    //         })
-    //     }
-    //     if (userHash.hulu == true) {
-    //         const huluLogo = document.createElement('img')
-    //         huluLogo.dataset.id = 2
-    //         huluLogo.src ="https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/bk8cux6dapq8qjzylfaj"
-    //         header.append(huluLogo)
-    //         huluLogo.addEventListener('click', event => {
-    //             event.preventDefault()
-    //             myMovieDiv.innerHTML = ""
-    //             userHash.user_movies.forEach(userMovie => {
-    //                 if (userMovie.movie.hulu == true) {
-    //                     const huluDiv = document.createElement('div')
-    //                     huluDiv.classList.add('hulucard')
-    //                     huluDiv.innerHTML =`<h2> ${userMovie.movie.title} </h2>
-    //                     <h3> Year Released: ${userMovie.movie.year} </h3>
-    //                     <img src ="${userMovie.movie.image}">
-    //                     <p> Likes: ${userMovie.movie.likes}</p>
-    //                     <p> Dislikes: ${userMovie.movie.dislikes}</p>
-    //                     `
-    //                     myMovieDiv.append(huluDiv)
-    //                 }
-    //             })
-
-    //         })
-    //     }
-    //     if (userHash.hbo == true) {
-    //         const hboLogo = document.createElement('img')
-    //         hboLogo.dataset.id = 3
-    //         hboLogo.src ="https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/lr6ntovb8hu36yqscb2c"
-    //         header.append(hboLogo)
-    //         hboLogo.addEventListener('click', event => {
-    //             event.preventDefault()
-    //             myMovieDiv.innerHTML = ""
-    //             userHash.user_movies.forEach(userMovie => {
-    //                 if (userMovie.movie.hbo == true) {
-    //                     const hboDiv = document.createElement('div')
-    //                     hboDiv.classList.add('hbocard')
-    //                     hboDiv.innerHTML =`<h2> ${userMovie.movie.title} </h2>
-    //                     <h3> Year Released: ${userMovie.movie.year} </h3>
-    //                     <img src ="${userMovie.movie.image}">
-    //                     <p> Likes: ${userMovie.movie.likes}</p>
-    //                     <p> Dislikes: ${userMovie.movie.dislikes}</p>
-    //                     `
-    //                     myMovieDiv.append(hboDiv)
-    //                 }
-    //             })
-
-    //         })
-    //     }
-    //     if (userHash.disney == true) {
-    //         const disneyLogo = document.createElement('img')
-    //         disneyLogo.dataset.id = 4
-    //         disneyLogo.src ="https://developer.asustor.com/uploadIcons/0020_999_1591957052_app_icon_Disney_plus_256.png"
-    //         header.append(disneyLogo)
-    //         disneyLogo.addEventListener('click', event => {
-    //             event.preventDefault()
-    //             myMovieDiv.innerHTML = ""
-    //             userHash.user_movies.forEach(userMovie => {
-    //                 if (userMovie.movie.disney == true) {
-    //                     const disneyDiv = document.createElement('div')
-    //                     disneyDiv.classList.add('disneycard')
-    //                     disneyDiv.innerHTML =`<h2> ${userMovie.movie.title} </h2>
-    //                     <h3> Year Released: ${userMovie.movie.year} </h3>
-    //                     <img src ="${userMovie.movie.image}">
-    //                     <p> Likes: ${userMovie.movie.likes}</p>
-    //                     <p> Dislikes: ${userMovie.movie.dislikes}</p>
-    //                     `
-    //                     myMovieDiv.append(disneyDiv)
-    //                 }
-    //             })
-
-    //         })
-    //     }
-    //     if (userHash.amazon == true) {
-    //         const amazonLogo = document.createElement('img')
-    //         amazonLogo.dataset.id = 5
-    //         amazonLogo.src ="https://apprecs.org/ios/images/app-icons/256/e5/545519333.jpg"
-    //         header.append(amazonLogo)
-    //         amazonLogo.addEventListener('click', event => {
-    //             event.preventDefault()
-    //             myMovieDiv.innerHTML = ""
-    //             userHash.user_movies.forEach(userMovie => {
-    //                 if (userMovie.movie.amazon == true) {
-    //                     const amazonDiv = document.createElement('div')
-    //                     amazonDiv.classList.add('amazoncard')
-    //                     amazonDiv.innerHTML =`<h2> ${userMovie.movie.title} </h2>
-    //                     <h3> Year Released: ${userMovie.movie.year} </h3>
-    //                     <img src ="${userMovie.movie.image}">
-    //                     <p> Likes: ${userMovie.movie.likes}</p>
-    //                     <p> Dislikes: ${userMovie.movie.dislikes}</p>
-    //                     `
-    //                     myMovieDiv.append(amazonDiv)
-    //                 }
-    //             })
-
-    //         })
-    //     }
-    // })
-// }
-
+// Puts movies from out database into movies(GV)
 function getAllMovies() {
     fetch('http://localhost:3000/movies')
     .then(response => response.json())
@@ -196,18 +19,16 @@ function getAllMovies() {
         movies = movieArray
     })
 }
+
+// Renders all movies from searched Movies
 function renderMovies(movieArray) {
     myMovieDiv.innerHTML = ""
         movieArray.forEach(movie => {
             renderMovie(movie)
         })
 }
-searchForm.addEventListener('input', event => {
-    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(event.target.value.toLowerCase()))
-    renderMovies(filteredMovies)
-})
 
-// Movies for the search
+// Creates movie div card for searched movies
 function renderMovie(movie) {
     const div = document.createElement('div')
     div.classList.add('moviecard')
@@ -226,9 +47,9 @@ function renderMyProfile() {
     .then(userHash => {
         headerName.innerText = `Profile: ${userHash.username}`
         renderUserLogos()
-        getAllMovies()
-        accessMyMovies()
-        renderMyMovies(myMovies)
+        // getAllMovies()
+        // accessMyMovies()
+        // renderMyMovies(myMovies)
     })
 }
 
@@ -280,12 +101,12 @@ function renderUserLogos() {
     })
 }
 
-// Render my UserMovies in 
+// Pushes UserMovies into myMovies(GV)
 function accessMyMovies(){
     fetch('http://localhost:3000/user_movies')
     .then(response => response.json())
     .then(userMovies => {
-        userMovies.map(userMovie => {
+        userMovies.forEach(userMovie => {
             if(userMovie.user_id === profileId){
               myMovies.push(userMovie)
             }
@@ -312,6 +133,38 @@ function renderMyMovies(movieArray){
     })
 }
 
+// Updates myMovies(GV) to show new likes
+function changeMyMoviesLikes(movie, value) {
+    for (const i in myMovies) {
+        if (myMovies[i].movie.title == movie.title) {
+            myMovies[i].movie.likes = value
+            break;
+        }
+    }
+}
+
+// Updates myMovies(GV) to show new dislikes
+function changeMyMoviesDislikes(movie, value) {
+    for (const i in myMovies) {
+        if (myMovies[i].movie.title == movie.title) {
+            myMovies[i].movie.dislikes = value
+            break;
+        }
+    }
+}
+
+// Updates myMovies(GV) to remove UserMovie object
+function changeMyMoviesDelete(movie) {
+    for (const i in myMovies) {
+        if (myMovies[i].id == movie.id) {
+            myMovies.splice(i, 1)
+            break;
+        }
+    }
+}
+
+/********** Event Listeners **********/
+
 // click to load my stream movies
 header.addEventListener('click', event => {
     myMovieDiv.innerHTML = ""
@@ -335,6 +188,12 @@ header.addEventListener('click', event => {
         const amazonMovies = myMovies.filter(userMovie => userMovie.movie.amazon)
         renderMyMovies(amazonMovies) 
         }
+})
+
+// search more from our database
+searchForm.addEventListener('input', event => {
+    const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(event.target.value.toLowerCase()))
+    renderMovies(filteredMovies)
 })
 
 // click to add move to watch list, like, dislike, and delete the movie from list
@@ -411,34 +270,12 @@ myMovieDiv.addEventListener('click', event => {
     }
 })
 
-function changeMyMoviesLikes(movie, value) {
-    for (const i in myMovies) {
-        if (myMovies[i].movie.title == movie.title) {
-            myMovies[i].movie.likes = value
-            break;
-        }
-    }
-}
+/********** Calling Functions **********/
 
-function changeMyMoviesDislikes(movie, value) {
-    for (const i in myMovies) {
-        if (myMovies[i].movie.title == movie.title) {
-            myMovies[i].movie.dislikes = value
-            break;
-        }
-    }
-}
-
-function changeMyMoviesDelete(movie) {
-    for (const i in myMovies) {
-        if (myMovies[i].id == movie.id) {
-            myMovies.splice(i, 1)
-            break;
-        }
-    }
-}
 renderMyProfile()
 getAllMovies()
-accessMyMovies()
-renderMyMovies(myMovies)
 
+document.addEventListener('DOMContentLoaded', () => {
+    accessMyMovies()
+    renderMyMovies(myMovies)
+})
